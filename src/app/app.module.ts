b79c451f1a4f9bdd925e82483ce1a527551b9753
Steven Lin <slin@merchantgrowth.com>
2019-11-19 10:52:49 -0800

    complete power boost calculatorPower

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FetchJsonPipe } from "./fetch-json.pipe";
+import { ExponentialStrengthPipe } from "./exponential-strength.pipe";
+import { HeroBirthdayComponent } from "./hero-birthday1.component";
+import { HeroBirthday2Component } from "./hero-birthday2.component";
+import { PowerBoosterComponent } from "./power-booster.component";
+import { PowerBoostCalculatorComponent } from "./power-boost-calculator.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroBirthdayComponent,
+    HeroBirthday2Component,
+    PowerBoosterComponent,
+    PowerBoostCalculatorComponent,
+    FetchJsonPipe,
+    ExponentialStrengthPipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/hero-birthday1.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-hero-birthday",
+  template: `
+    <p>The hero's birthday is {{ birthday | date }}</p>
+  `
+})
+export class HeroBirthdayComponent {
+  birthday = new Date(1988, 3, 15);
+
+  constructor() {
+    //console.log(this.birthday);
+  }
+}
